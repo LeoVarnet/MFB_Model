a44fe5c8ae023f324de2cbfa049d64d9d618e1fa
LeoVarnet <44163905+LeoVarnet@users.noreply.github.com>
2018-10-15 14:09:04 +0100

    Adding useful functions

---
+++ plot_channels.m
@@ -0,0 +1,55 @@
+function [ ] = plot_channels( X, Y, chan2plot, plotfun, msubplot, nsubplot )
+%PLOT_CHANNELS Conveniently plots a certain number of channels in a matrix
+% of filterbank responses. To be used as plot_channels( X, Y, chan2plot)
+%
+% plot_channels( X, Y, chan2plot, plotfun, msubplot, nsubplot) uses the
+% plotting function in plotfun (default: plot) and creates a
+% msubplot-by-nsubplot array of plots
+%
+% Leo Varnet 2016
+
+Nplot = length(chan2plot);
+if nargin<4
+    plotfun = @plot;
+end
+if nargin<6
+    nsubplot=ceil(sqrt(Nplot));
+    if nsubplot*(nsubplot-2)>=Nplot
+        msubplot=nsubplot-2;
+    elseif nsubplot*(nsubplot-1)>=Nplot
+        msubplot=nsubplot-1;
+    else
+        msubplot=nsubplot;
+    end
+end
+if nargin>6
+    error('You must specify both msubplot and nsubplot: plot_channels( X, Y, chan2plot, plotfun, msubplot, nsubplot )');
+end
+
+%figure;
+
+if ndims(Y)==2
+    maxval = max(max(abs(Y(:,chan2plot))));
+elseif ndims(Y)==3
+    maxval = max(max(max(abs(Y(:,chan2plot,:)))));
+end
+
+isubplot = 1;
+for ichan=chan2plot
+    subplot(nsubplot,msubplot,isubplot);
+    if ndims(Y)==2
+        plotfun(X,Y(:,ichan));
+        maxval = max(max(abs(Y(:,chan2plot))));
+    elseif ndims(Y)==3
+        plotfun(X,squeeze(Y(:,ichan,:)));
+    end
+    title(['Channel #' num2str((ichan))]);
+    
+    xlim([X(1) X(end)]); ylim([-maxval maxval]);
+    isubplot = isubplot+1;
+    hold on
+end
+%xlabel('time (s)');
+
+end
+
---
+++ plot_sound.m
@@ -0,0 +1,104 @@
+function [pdB, f_spec, t_spec] = plot_sound( S, fs, varargin )
+%PLOT_SOUND ( S, fs, Freq, Overlap, Nwindow, NFFT, Time, dBlim )
+% wavform and spectrographic display of sound S. Spectrogram has parameters
+% Freq (frequency range, default [0, 8000]), Overlap (default 0.5), Nwindow
+% (default 512), NFFT (default 1024). Time can be used optionnaly to limit
+% the sound to an interval. 
+% Optionally, you can get the spectrogram as an output:
+% [pdB, f_spec, t_spec] = plot_sound( S, fs, varargin )
+%
+% Leo Varnet - 2018
+
+% defaults
+dBlim = []; 
+Time = []; 
+Freq = [0 8000]; 
+Overlap = 50/100;
+Nwindow = 512;
+NFFT = 1024;
+
+if length(varargin)>=6 & ~isempty(varargin{6})
+    dBlim = varargin{6};
+end
+if length(varargin)>=5 & ~isempty(varargin{5})
+    Time = varargin{5};
+end
+if length(varargin)>=4 & ~isempty(varargin{4})
+    NFFT = varargin{4};
+end
+if length(varargin)>=3 & ~isempty(varargin{3})
+    Nwindow = varargin{3};
+end
+if length(varargin)>=2 & ~isempty(varargin{2})
+    Overlap = varargin{2};
+end
+if length(varargin)>=1 & ~isempty(varargin{1})
+    Freq = varargin{1};
+end
+
+if ~isempty(Time)
+    if Time(1)==0
+        n1_ech=1;
+    else
+        n1_ech=Time(1)*fs;
+    end
+    if Time(2)*fs<length(S)
+        n2_ech=Time(2)*fs;
+    else
+        n2_ech=length(S);
+    end
+else 
+    n1_ech = 1;
+    n2_ech = length(S);
+end
+
+S=S(n1_ech:n2_ech);
+t_S = (1:length(S))/fs;
+
+figure
+ht = subplot(2,2,1,'Position',[0.1, 0.75, 0.6, 0.2]);
+plot(ht, t_S,S);
+xlim(ht, [t_S(1) t_S(end)])
+ylabel(ht,'Amplitude')
+set(ht, 'XTickLabels',{})
+
+load('AuditionColorbar.mat');
+
+[~,f_spec,t_spec,p] = spectrogram(S,Nwindow,Nwindow*Overlap,NFFT,fs);
+pdB=10*log10(abs(p));
+FreqIndex=find(f_spec>=Freq(1) & f_spec<=Freq(2));
+
+[perio, f_perio] = periodogram(S, 1:length(S), f_spec, fs);
+hf = subplot(2,2,4,'Position', [0.75, 0.1, 0.2, 0.6]);
+perio_dB = 10*log10((perio));%abs
+plot(hf, f_perio, perio_dB);
+xlim(hf, Freq)
+ylabel(hf, 'amplitude (dB)');
+set(hf, 'XTickLabels',{})
+% set(hf, 'YTick',[-150 -100 -70 -50])
+% set(hf, 'YTickLabels',{'0','50', '80', '100'})
+camroll(hf, 90)
+
+hs = subplot(2,2,3,'Position',[0.1,0.1, 0.6, 0.6]);
+%scrsz = get(groot,'ScreenSize');
+%figure('Position',[0 scrsz(4)-200-100 1300 200]);
+surf(t_spec,f_spec(FreqIndex),pdB(FreqIndex,:),'EdgeColor','none');
+surf(hs, t_spec,f_spec(FreqIndex),pdB(FreqIndex,:),'EdgeColor','none');
+h=pcolor(t_spec,f_spec(FreqIndex),pdB(FreqIndex,:));
+
+if ~isempty(dBlim)
+    caxis(dBlim)
+end
+set(h,'EdgeColor','none');
+axis xy; axis tight; view(0,90);
+xlim(hs, [t_S(1) t_S(end)])%xlim(hs,[t_spec(1) t_spec(end)])
+ylim(hs, Freq)
+xlabel('time (s)'); ylabel('frequency (Hz)');
+colormap(Audition);
+
+set(ht, 'Position', [0.1, 0.75, 0.6, 0.2]);
+set(hf, 'Position', [0.75, 0.1, 0.2, 0.6]);
+set(hs, 'Position', [0.1, 0.1, 0.6, 0.6]);
+
+end
+
